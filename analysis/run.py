b45a548fb2ad2e36c4e43c8edf2397162afd0ba0
sjvintage <sjvintage@gmail.com>
2014-02-11 16:33:38 -0800

    [analysis] Make program more verbose…

--- analysis/run.py
+++ analysis/run.py
@@ -1,4 +1,5 @@
 import settings
+import time
 
 from modules.multimap_filter import MultimapFilter
 from modules.read_counter import ReadCounter
@@ -8,18 +9,33 @@ from os import listdir, path
 def main():
     config = settings.Settings
 
+    print "\nInitializing read counter..."
+
     rc = ReadCounter(config.gene_boundaries, config.bin_size)
 
     for f in listdir(config.alignments_dir):
         sampleid = f.strip("_alignments.txt")
 
+        print "\nCounting reads for sample %s..." % sampleid
+
         rc.init_sample(sampleid)
 
+        count = 0
         for alignment in MultimapFilter(path.join(config.alignments_dir, f)).get_unique_mapped():
             rc.log_alignment(sampleid, alignment)
+
+            count += 1
+
+        print "%i unique alignments logged for sample %s." % (count, sampleid)
+
+    print "\nWriting read counts table to %s..." % config.output_file
 
     rc.write_counter(config.output_file)
 
 
 if __name__ == "__main__":
+    start_time = time.time()
+
     main()
+
+    print "\nScript completed in %i seconds." % (time.time() - start_time)
