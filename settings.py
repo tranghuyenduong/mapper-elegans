eb57ee6d422a1432aaf8dd5209164d3b716927df
sjvintage <sjvintage@gmail.com>
2014-03-27 18:39:52 -0700

    Update paths to refs

--- settings.py
+++ settings.py
@@ -1,15 +1,16 @@
 class RefsConfig():
 
-    genome = "/home/sba/noelle/ws240/c_elegans.PRJNA13758.WS240.genomic.fa"
-    gff2_records = "/home/sba/noelle/ws240/c_elegans.PRJNA13758.WS240.annotations.gff2"
-    gff3_records = "/home/sba/noelle/ws240/c_elegans.PRJNA13758.WS240.annotations.gff3"
-    gene_ids = "/home/sba/noelle/ws240/c_elegans.PRJNA13758.WS240.geneIDs.txt"
-    ref = "/home/sba/noelle/refs"
+    genome = "/home/sba/bioinf/noelle/ws240/c_elegans.PRJNA13758.WS240" \
+             ".genomic.fa"
+    gff2_records = "/home/sba/bioinf/noelle/ws240/c_elegans.PRJNA13758.WS240.annotations.gff2"
+    gff3_records = "/home/sba/bioinf/noelle/ws240/c_elegans.PRJNA13758.WS240.annotations.gff3"
+    gene_ids = "/home/sba/bioinf/noelle/ws240/c_elegans.PRJNA13758.WS240.geneIDs.txt"
+    ref = "/home/sba/bioinf/noelle/refs"
 
 
 class PreprocessConfig():
 
-    raw_input = "/home/sba/noelle/reads/WT_Odor_3.fastq"
+    raw_input = "/home/sba/bioinf/noelle/reads/WT_Odor_3.fastq"
     force_preprocess = False
 
     scriptminer = True
@@ -28,31 +29,31 @@ class PreprocessConfig():
 
 class BowtieConfig():
 
-    genome_ref = "/home/sba/noelle/refs/bt/genome"
-    coding_transcripts_ref = "/home/sba/noelle/refs/bt/coding_transcripts"
-    transcript_bed = "/home/sba/noelle/refs/primary_transcripts.bed"
-    transcript_parents = "/home/sba/noelle/refs/transcript_parents"
-    exon_coords = "/home/sba/noelle/refs/exon_coords"
+    genome_ref = "/home/sba/bioinf/noelle/refs/bt/genome"
+    coding_transcripts_ref = "/home/sba/bioinf/noelle/refs/bt/coding_transcripts"
+    transcript_bed = "/home/sba/bioinf/noelle/refs/primary_transcripts.bed"
+    transcript_parents = "/home/sba/bioinf/noelle/refs/transcript_parents"
+    exon_coords = "/home/sba/bioinf/noelle/refs/exon_coords"
 
 
 class PostprocessConfig():
 
-    pirna_mirna_records = "/home/sba/noelle/refs/pirna_mirna"
+    pirna_mirna_records = "/home/sba/bioinf/noelle/refs/pirna_mirna"
 
 
 class GeneIntersectConfig():
 
-    genes = "/home/sba/noelle/refs/genes"
+    genes = "/home/sba/bioinf/noelle/refs/genes"
 
 
 class Output():
 
-    alignments = "/home/sba/noelle/alignments/WT_Odor_3_alignments.txt"
+    alignments = "/home/sba/bioinf/noelle/alignments/WT_Odor_3_alignments.txt"
 
 
 class AnalysisConfig():
 
-    genes = "/home/sba/noelle/refs/genes"
+    genes = "/home/sba/bioinf/noelle/refs/genes"
     bin_size = 400
-    alignments_dir = "/home/sba/noelle/alignments"
-    output_file = "/home/sba/noelle/data_400.csv"
+    alignments_dir = "/home/sba/bioinf/noelle/alignments"
+    output_file = "/home/sba/bioinf/noelle/data_400.csv"
