04db4bdead3846ec093621394d17ec6bbf547fa6
sjvintage <sjvintage@gmail.com>
2014-07-02 12:14:02 -0700

    Change the way paths are handled when generating refs. Add a provision to extract exon records.

--- refs.py
+++ refs.py
@@ -13,48 +13,61 @@ def wrapper():
     if not os.path.exists(config.ref):
         os.makedirs(config.ref)
 
+    def P(path, base_dir = config.ref):
+        return os.path.join(base_dir, path)
+
     # Extract protein coding genes
     print "\nExtracting protein coding genes..."
-    modules.protein_coding_genes(config.gff3_records, config.ref + "/genes")
+    modules.protein_coding_genes(config.gff3_records, P("genes"))
 
     # Extract primary transcripts
     print "\nExtracting primary transcripts..."
-    modules.extract_bed(config.gff2_records, config.ref + "/primary_transcripts.bed", "protein_coding_primary_transcript")
-    modules.extract_sequences(config.genome, config.ref + "/primary_transcripts.bed", config.ref + "/primary_transcripts.fa")
+    modules.extract_bed(config.gff2_records, P("primary_transcripts.bed"),
+        "protein_coding_primary_transcript")
+    modules.extract_sequences(config.genome, P("primary_transcripts.bed"),
+        P("primary_transcripts.fa"))
 
     # Extract introns
     print "\nExtracting introns..."
-    modules.extract_bed(config.gff2_records, config.ref + "/introns.bed", "intron")
-    modules.order_records(config.ref + "/introns.bed", config.ref + "/introns_sorted.bed")
-    modules.extract_sequences(config.genome, config.ref + "/introns_sorted.bed", config.ref + "/introns_sorted.fa")
+    modules.extract_bed(config.gff2_records, P("introns.bed"), "intron")
+    modules.order_records(P("introns.bed"), P("introns_sorted.bed"))
+    modules.extract_sequences(config.genome, P("introns_sorted.bed"),
+        P("introns_sorted.fa"))
+
+    # Extract coding exons
+    print "\nExtracting exons..."
+    modules.extract_bed(config.gff2_records, P("exons.bed"), "exon")
 
     # Extract transcript parents
     print "\nExtracting transcript parents..."
-    modules.transcript_parents(config.gff3_records, config.ref + "/transcript_parents")
+    modules.transcript_parents(config.gff3_records, P("transcript_parents"))
 
     # Extract piRNA/miRNA records
     print "\nExtracting piRNA and miRNA records..."
-    modules.pirna_mirna_records(config.gff3_records, config.ref + "/pirna_mirna")
+    modules.pirna_mirna_records(config.gff3_records, P("pirna_mirna"))
 
     # Extract public names for transcripts
     print "\nExtracting gene public names..."
-    modules.public_names(config.gene_ids, config.ref + "/public_names")
+    modules.public_names(config.gene_ids, P("public_names"))
 
     # Extract sequence ids for transcripts
     print "\nExtracting gene sequence ids..."
-    modules.sequence_ids(config.gene_ids, config.ref + "/sequence_ids")
+    modules.sequence_ids(config.gene_ids, P("sequence_ids"))
 
     # Part primary transcripts
     print "\nParting primary transcripts..."
-    modules.part_primary_transcripts(config.ref + "/introns_sorted.fa", config.ref + "/primary_transcripts.fa", config.ref + "/parted_primary_transcripts")
+    modules.part_primary_transcripts(P("introns_sorted.fa"),
+        P("primary_transcripts.fa"), P("parted_primary_transcripts"))
 
     # Generate coding transcripts
     print "\nGenerating coding transcripts..."
-    modules.coding_transcripts(config.ref + "/introns_sorted.fa", config.ref + "/parted_primary_transcripts", config.ref + "/coding_transcripts.fa")
+    modules.coding_transcripts(P("introns_sorted.fa"),
+        P("parted_primary_transcripts"), P("coding_transcripts.fa"))
 
     # Extract exon coordinates relative to primary transcript
     print "\nExtracting exon coordinates from primary transcript..."
-    modules.exon_coords(config.ref + "/introns_sorted.fa", config.ref + "/parted_primary_transcripts", config.ref + "/exon_coords")
+    modules.exon_coords(P("introns_sorted.fa"),
+        P("parted_primary_transcripts"), P("exon_coords"))
 
     # Generate bowtie indices for genome, primary transcripts, and coding transcripts
     print "\nBuilding the necessary bowtie indices..."
@@ -63,8 +76,9 @@ def wrapper():
     bt_indices = config.ref + "/bt"
     if not os.path.exists(bt_indices):
         os.makedirs(bt_indices)
-    modules.build_bowtie_index(config.genome, bt_indices + "/genome")
-    modules.build_bowtie_index(config.ref + "/coding_transcripts.fa", bt_indices + "/coding_transcripts")
+    modules.build_bowtie_index(config.genome, P("genome", bt_indices))
+    modules.build_bowtie_index(P("coding_transcripts.fa"),
+        P("coding_transcripts", bt_indices))
     print "\nReference files generated in %s." % config.ref
 
 
