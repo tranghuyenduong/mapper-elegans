a169c42106d550c31c71b30e2d23a98160ea783a
Jeev <me@jeev.co>
2015-05-09 18:08:54 -0700

    Keep track of start and end coords of gene bins

--- modules/ReadCounter/__init__.py
+++ modules/ReadCounter/__init__.py
@@ -18,18 +18,19 @@ class ReadCounter():
         adj_bin_size = int(ceil(float(end-start) / float(bin_count)))
 
         for idx in xrange(start, end, adj_bin_size):
-            yield idx
+            yield idx, idx + adj_bin_size - 1
 
     def _create_bins(self, genes):
         for record in Bed.parse(open(genes, "rU")):
-            for bin in self._get_bins(record.chrom_start, record.chrom_end):
-                self.counter[record.name, bin] = defaultdict(int)
-                self.bins[record.name].append(bin)
+            for start, end in self._get_bins(record.chrom_start,
+                                             record.chrom_end):
+                self.counter[record.name, start, end] = defaultdict(int)
+                self.bins[record.name].append((start, end))
 
     def _find_bin(self, geneid, coord):
-        for bin in reversed(self.bins[geneid]):
-            if bin <= coord:
-                return geneid, bin
+        for start, end in reversed(self.bins[geneid]):
+            if start <= coord:
+                return geneid, start, end
 
     def init_sample(self, sampleid):
         self.samples.append(sampleid)
@@ -38,16 +39,19 @@ class ReadCounter():
         self.counter[self._find_bin(gene, coord)][sampleid] += score
 
     def write_counter(self, output):
-        counter = sorted(self.counter.iteritems(), key=lambda s: (s[0][0], s[0][1]))
+        counter = sorted(self.counter.iteritems(),
+                         key=lambda s: (s[0][0], s[0][1]))
 
         self.samples.sort()
 
         with open(output, "w") as output_handle:
             output_handle.write("Gene:Bin,%s\n" % ",".join(self.samples))
 
-            for (geneid, bin), count_dict in counter:
-                output_handle.write("%s:%i,%s\n" % (
+            for (geneid, start, end), count_dict in counter:
+                output_handle.write("%s:%i-%i,%s\n" % (
                     geneid,
-                    bin,
-                    ",".join(str(count_dict[sample]) for sample in self.samples)
+                    start,
+                    end,
+                    ",".join(str(count_dict[sample]) \
+                             for sample in self.samples)
                 ))
